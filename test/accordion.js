6212eaa1d7f7aa2fe7c824db5f9fb4c1bb27cf0a
rajasegar-c <rajasegarc@freshdesk.com>
2016-10-16 15:56:22 +0530

    New navbar mixin for using brand images

--- test/accordion.js
+++ test/accordion.js
@@ -12,7 +12,6 @@ describe("Accordion",function() {
     var fn = jade.compileFile(path.join(__dirname,"fixtures/accordion","accordion.jade"));
     var actual = '<div id="accordionmyaccordion" role="tablist" aria-multiselectable="true" class="panel-group"><div class="panel panel-primary"><div role="tab" id="headingMy Accordion Item" class="panel-heading"><h4 class="panel-title"><a data-toggle="collapse" data-parent="#accordionmyaccordion" href="#collapseMyAccordionItem" aria-expanded="true" aria-controls="collapseOne">My Accordion Item</a></h4></div><div id="collapseMyAccordionItem" role="tabpanel" aria-labelledby="headingMy Accordion Item" class="panel-collapse collapse"><div class="panel-body"></div></div></div></div>';
     var locals = { id: "myaccordion",title:"My Accordion Item",type: "primary", parent: "myaccordion",expanded:false};
-    console.log(fn(locals));
     it("should generate an accordion",function() {
         assert(actual,fn(locals));
     });

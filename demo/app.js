bf16a3e0c17847f0c6d86b6163f1ee99e54539e3
Rajasegar <rajasegar.c@gmail.com>
2020-11-29 16:55:07 +0530

    chore: add badge mixins
    1. add separate routes for alerts and badges
    2. tweak layout of demo page

--- demo/app.js
+++ demo/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var alertsRouter = require('./routes/alerts');
+var badgesRouter = require('./routes/badges');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/alerts', alertsRouter);
+app.use('/badges', badgesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
